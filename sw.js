e14c4e7669b69b1d0f40cb72ee684bf3d017a686
bluestarlights <118288571+bluestarlights@users.noreply.github.com>
2026-09-12 04:51:31 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // V7: 앱 범위 캐시만 관리하고 사용자 localStorage는 변경하지 않는다.
-const CACHE='bp-v7-mobile-20260912-2';
+const CACHE='bp-v7-mobile-20260912-3';
 const ASSETS=['./','./index.html','./manifest.json','./icon.svg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',event=>{event.waitUntil((async()=>{for(const name of await caches.keys())if(/^bp-v[4-7]-/.test(name)&&name!==CACHE)await caches.delete(name);await self.clients.claim()})())});
